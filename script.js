9a6993b800834b26d2e74274d71a767f8c982cc9
Tapan Karangiya <99003457+tapan111@users.noreply.github.com>
2024-01-20 17:26:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const video = document.getElementById('video')
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+  
+ 
+]).then(startWebcam)
+
+function startWebcam() {
+  navigator.mediaDevices
+  .getUserMedia(
+    { 
+      video:true,
+      audio: false,
+     }
+    
+  ).then((stream)=>{
+    video.srcObject=stream;
+  })
+  .catch((error)=>{
+    console.error(error);
+  })
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    
+  }, 100)
+})
